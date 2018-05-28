139bcf5d63d819b0d04ead84727c8a812b4593df
Shadow-Step <Shadow-Step@mail.ru>
2018-05-28 11:09:27 +0300

    asd

---
+++ LittleHelper/LittleHelper/model/Buttons.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LittleHelper.model;
+
+namespace LittleHelper.model
+{
+    class Buttons
+    {
+        public static Coords MAP_BUTTON;
+        public static Coords VILLAGE_BUTTON;
+        public static Coords PARISH_BUTTON;
+        public static Coords RESEARCH_BUTTON;
+        public static Coords RANK_BUTTON;
+        public static Coords QUEST_BUTTON;
+        public static Coords ATTACK_BUTTON;
+        public static Coords REPORTS_BUTTON;
+        public static Coords FRACTION_BUTTON;
+
+    }
+}
---
+++ LittleHelper/LittleHelper/model/Coords.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LittleHelper.model
+{
+    class Coords
+    {
+        int X { get; set; }
+        int Y { get; set; }
+        
+        public Coords(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
